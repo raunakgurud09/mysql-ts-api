18407e0bf4a62fbb40646d3d374cbd557f356b98
Raunak Gurud <raunakgurud2121@gmail.com>
2024-02-12 23:21:02 +0530

    seed the db

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,34 @@
 import express, { Request, Response } from "express";
 import mysql from "mysql";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
+
+const connectionString = process.env.DATABASE_URL || "";
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "password",
+});
+connection.connect();
 
 const PORT = process.env.PORT || 8080;
 
 app.get("/api/char", (req: Request, res: Response) => {
-  res.send("It works");
+  const query = "SELECT * FROM Characters";
+  connection.query(query, (err, rows) => {
+    if (err) throw err;
+
+    const retVal = {
+      data: rows,
+      message: "",
+    };
+    if (rows.length === 0) {
+      retVal.message = "No records found";
+    }
+    res.send(rows);
+  });
 });
 
 app.get("/api/char/:id", (req: Request, res: Response) => {
